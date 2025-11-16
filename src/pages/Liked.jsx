e5409d266a5fed9d05635a1fcc853bd934ade422
Madina <madinaabduraximova2007@gmail.com>
2025-11-16 19:48:12 +0500

    i added img for no liked posts part inside liked page

--- src/pages/Liked.jsx
+++ src/pages/Liked.jsx
@@ -3,6 +3,7 @@ import eye from "../assets/eye2.svg";
 import Recommended from "../components/Recommended";
 import { Newcontext } from "../Newcontext/Newcontext";
 import { useTranslation } from "react-i18next";
+import noData from "../assets/no-data.png"
 const Liked = () => {
    const {t} = useTranslation();
   const {liked} = useContext(Newcontext);
@@ -10,18 +11,20 @@ const Liked = () => {
 
   return (
     <section className="container liked-pg">
-      {liked.length == 0 && <h1>{t("noLiked.no")}</h1>}
+      {liked.length == 0 && (
+        <div style={{ display:`flex`, flexDirection:`column`,  alignItems:`center`, marginLeft:`180px`}}>
+          <img data-v-95589494="" style={{height:`250px`, width:`250px`}} src={noData} class="empty-page__image" />
+          <h4 className="noLiked">{t("noLiked.no")}</h4>
+        </div>
+      )}
       {liked.length > 0 && (
         <div className="liked-cards">
-          {
-          liked?.map((item) => (
+          {liked?.map((item) => (
             <div className="liked-card" key={item.id}>
               <div className="liked-card-img"></div>
               <div className="liked-card-right">
                 <h6 className="liked-card-title">{item.title}</h6>
-                <p className="liked-card-description">
-        {item.body}
-                </p>
+                <p className="liked-card-description">{item.body}</p>
                 <div className="card-bottom">
                   <div className="topic-of-card">
                     <img src={eye} className="eye" alt="" />
